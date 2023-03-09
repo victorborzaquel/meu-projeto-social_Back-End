e4a80a7587bbfba25009b59839215f1c022c60a6
Victor Borzaquel <victorborzaquel@outlook.com>
2023-03-09 15:27:29 -0300

    Initial commit

---
+++ src/main/java/br/com/meuprojetosocial/meuprojetosocial/MeuProjetoSocialApplication.java
@@ -0,0 +1,13 @@
+package br.com.meuprojetosocial.meuprojetosocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuProjetoSocialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meuprojetosocial/meuprojetosocial/MeuProjetoSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuprojetosocial.meuprojetosocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
